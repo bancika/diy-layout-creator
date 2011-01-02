77cb69e62a5bb40865e55eafea6e198c08baa1a4
bancika <bancika@813357f0-ed8a-11de-91ba-91c70a792394>
2011-01-02 18:12:35 +0000

    added new src for library

---
+++  diy-layout-creator --username bancika@gmail.com/DIYLC/src.library/com/diyfever/diylc/components/ResistorType.java
@@ -0,0 +1,42 @@
+package com.diyfever.diylc.components;
+
+import javax.swing.Icon;
+
+import com.diyfever.diylc.images.IconLoader;
+import com.diyfever.diylc.model.ComponentLayer;
+import com.diyfever.diylc.model.IComponentInstance;
+import com.diyfever.diylc.model.IComponentType;
+
+public class ResistorType implements IComponentType {
+
+	@Override
+	public Class<? extends IComponentInstance> getComponentInstanceClass() {
+		return ResistorInstance.class;
+	}
+
+	@Override
+	public ComponentLayer getComponentLayer() {
+		return ComponentLayer.COMPONENT;
+	}
+
+	@Override
+	public String getDescription() {
+		return "Some description";
+	}
+
+	@Override
+	public Icon getIcon() {
+		return IconLoader.Gears.getIcon();
+	}
+
+	@Override
+	public String getName() {
+		return "Resistor";
+	}
+
+	@Override
+	public String getCategory() {
+		return "Samples";
+	}
+
+}
