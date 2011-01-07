9b34832de4ef440bcf6582b396f8a8a0b6830658
bancika <bancika@813357f0-ed8a-11de-91ba-91c70a792394>
2011-01-07 17:03:39 +0000

    added boards

---
+++ trunk/ diy-layout-creator --username bancika@gmail.com/Library/src/org/diylc/components/BlankBoard.java
@@ -0,0 +1,31 @@
+package org.diylc.components;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+import org.diylc.core.ComponentLayer;
+import org.diylc.core.annotations.ComponentDescriptor;
+
+@ComponentDescriptor(name = "Blank Board", category = "Boards", author = "Branislav Stojkovic", componentLayer = ComponentLayer.BOARD, instanceNamePrefix = "B", desciption = "Blank FR4 board")
+public class BlankBoard extends AbstractBoard {
+
+	private static final long serialVersionUID = 1L;
+
+	public static Color BOARD_COLOR = Color.decode("#BCED91");
+	public static Color BORDER_COLOR = BOARD_COLOR.darker();
+
+	@Override
+	protected Color getBoardColor() {
+		return BOARD_COLOR;
+	}
+
+	@Override
+	protected Color getBorderColor() {
+		return BORDER_COLOR;
+	}
+
+	@Override
+	public void drawIcon(Graphics2D g2d, int width, int height) {
+
+	}
+}
