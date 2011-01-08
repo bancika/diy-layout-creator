7484067926a74ce83f40c80f8c53d4b041d547fa
bancika <bancika@813357f0-ed8a-11de-91ba-91c70a792394>
2011-01-08 18:19:57 +0000

    Added jumper and trace

---
+++ trunk/ diy-layout-creator --username bancika@gmail.com/Library/src/org/diylc/components/interconnects/Trace.java
@@ -0,0 +1,96 @@
+package org.diylc.components.interconnects;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Shape;
+
+import org.diylc.components.AbstractLeadedDIYComponent;
+import org.diylc.core.annotations.ComponentDescriptor;
+import org.diylc.core.annotations.EditableProperty;
+import org.diylc.core.measures.Size;
+import org.diylc.core.measures.SizeUnit;
+
+@ComponentDescriptor(name = "Trace", author = "Branislav Stojkovic", category = "Interconnects", instanceNamePrefix = "J", desciption = "Jumper wire")
+public class Trace extends AbstractLeadedDIYComponent<Void> {
+
+	private static final long serialVersionUID = 1L;
+
+	public static Size THICKNESS = new Size(1d, SizeUnit.mm);
+	public static Color COLOR = Color.black;
+
+	private Color leadColor = COLOR;
+	private Size leadThickness = THICKNESS;
+
+	@Override
+	public void drawIcon(Graphics2D g2d, int width, int height) {
+
+	}
+
+	@Override
+	@EditableProperty(name = "Color")
+	public Color getLeadColor() {
+		return leadColor;
+	}
+
+	public void setLeadColor(Color leadColor) {
+		this.leadColor = leadColor;
+	}
+
+	@Override
+	@EditableProperty(name = "Thickness")
+	public Size getLeadThickness() {
+		return leadThickness;
+	}
+
+	public void setLeadThickness(Size leadThickness) {
+		this.leadThickness = leadThickness;
+	}
+
+	@Deprecated
+	@Override
+	public Size getWidth() {
+		return super.getWidth();
+	}
+
+	@Deprecated
+	@Override
+	public Size getHeight() {
+		return super.getHeight();
+	}
+
+	@Deprecated
+	@Override
+	public Void getValue() {
+		return null;
+	}
+
+	@Deprecated
+	@Override
+	public void setValue(Void value) {
+	}
+
+	@Override
+	protected Color getBodyColor() {
+		return null;
+	}
+
+	@Override
+	protected Color getBorderColor() {
+		return null;
+	}
+
+	@Override
+	protected Shape getComponentShape() {
+		return null;
+	}
+
+	@Override
+	protected Size getDefaultHeight() {
+		return null;
+	}
+
+	@Override
+	protected Size getDefaultWidth() {
+		return null;
+	}
+}
