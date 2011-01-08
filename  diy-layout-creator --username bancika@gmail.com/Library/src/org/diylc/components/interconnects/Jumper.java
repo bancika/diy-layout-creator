f3bc414676933371a13add9fe5deb453a8accc82
bancika <bancika@813357f0-ed8a-11de-91ba-91c70a792394>
2011-01-08 18:19:57 +0000

    Added jumper and trace

---
+++  diy-layout-creator --username bancika@gmail.com/Library/src/org/diylc/components/interconnects/Jumper.java
@@ -0,0 +1,68 @@
+package org.diylc.components.interconnects;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Shape;
+
+import org.diylc.components.AbstractLeadedDIYComponent;
+import org.diylc.core.annotations.ComponentDescriptor;
+import org.diylc.core.measures.Size;
+
+@ComponentDescriptor(name = "Jumper", author = "Branislav Stojkovic", category = "Interconnects", instanceNamePrefix = "J", desciption = "Jumper wire")
+public class Jumper extends AbstractLeadedDIYComponent<Void> {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void drawIcon(Graphics2D g2d, int width, int height) {
+
+	}
+	
+	@Deprecated
+	@Override
+	public Size getWidth() {
+		return super.getWidth();
+	}
+
+	@Deprecated
+	@Override
+	public Size getHeight() {
+		return super.getHeight();
+	}
+
+	@Deprecated
+	@Override
+	public Void getValue() {
+		return null;
+	}
+
+	@Deprecated
+	@Override
+	public void setValue(Void value) {
+	}
+
+	@Override
+	protected Color getBodyColor() {
+		return null;
+	}
+
+	@Override
+	protected Color getBorderColor() {
+		return null;
+	}
+
+	@Override
+	protected Shape getComponentShape() {
+		return null;
+	}
+
+	@Override
+	protected Size getDefaultHeight() {
+		return null;
+	}
+
+	@Override
+	protected Size getDefaultWidth() {
+		return null;
+	}
+}
