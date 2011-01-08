3bad8a56adb32ceb7341f86dbdfbdeef35bef805
bancika <bancika@813357f0-ed8a-11de-91ba-91c70a792394>
2011-01-08 18:31:36 +0000

    cleanup

--- trunk/ diy-layout-creator --username bancika@gmail.com/DIYLC/src/org/diylc/DIYLCStarter.java
+++ trunk/ diy-layout-creator --username bancika@gmail.com/DIYLC/src/org/diylc/DIYLCStarter.java
@@ -14,11 +14,15 @@ import org.diylc.presenter.Presenter;
 import com.diyfever.gui.miscutils.PropertyInjector;
 
 /**
+ * Main class that runs DIYLC.
  * 
  * @author Branislav Stojkovic
+ * 
+ * @see Presenter
+ * @see MainFrame
  */
 public class DIYLCStarter {
-	
+
 	private static final Logger LOG = Logger.getLogger(Presenter.class);
 
 	/**
@@ -50,61 +54,7 @@ public class DIYLCStarter {
 		}
 
 		MainFrame mainFrame = new MainFrame();
-		// Presenter p = new Presenter(mainFrame);
-		//
-		// p.installPlugin(new ToolBox());
-		// p.installPlugin(new FileManager());
-		// p.installPlugin(new ClipboardManager());
-		// p.installPlugin(new StatusBar());
-		// p.installPlugin(new CanvasPlugin());
-		//
-		// Project project = new Project();
-		// project.getComponents().add(new MockDIYComponent());
-		// p.loadProject(project);
-		// panel.setPreferredSize(new Dimension(2000, 1500));
-		// RulerScrollPane scrollPane = new RulerScrollPane(panel);
-		// JFrame f = new JFrame("Test");
-		// f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		// f.add(scrollPane);
-		// f.setPreferredSize(new Dimension(800, 600));
-		// f.pack();
-		// f.setLocationRelativeTo(null);
 		mainFrame.setLocationRelativeTo(null);
 		mainFrame.setVisible(true);
-
-		// List<Property> properties = ClassProcessor
-		// .extractProperties(MockComponent.class);
-		// MockComponent component = new MockComponent();
-		// for (Property p : properties) {
-		// try {
-		// p.readFrom(component);
-		// } catch (IllegalArgumentException e) {
-		// // TODO Auto-generated catch block
-		// e.printStackTrace();
-		// } catch (IllegalAccessException e) {
-		// // TODO Auto-generated catch block
-		// e.printStackTrace();
-		// } catch (InvocationTargetException e) {
-		// // TODO Auto-generated catch block
-		// e.printStackTrace();
-		// }
-		// }
-		// PropertyEditorDialog.showFor(null, properties);
-		// for (Property p : properties) {
-		// try {
-		// p.writeTo(component);
-		// } catch (IllegalArgumentException e) {
-		// // TODO Auto-generated catch block
-		// e.printStackTrace();
-		// } catch (IllegalAccessException e) {
-		// // TODO Auto-generated catch block
-		// e.printStackTrace();
-		// } catch (InvocationTargetException e) {
-		// // TODO Auto-generated catch block
-		// e.printStackTrace();
-		// }
-		// System.out.println(p);
-		// }
-		// System.out.println(p.getComponentTypes());
 	}
 }
