e7784f442b2ffb073851399f8e8cee229699ffa6
bancika <bancika@813357f0-ed8a-11de-91ba-91c70a792394>
2011-01-10 00:07:22 +0000

    added abstract curve

---
+++  diy-layout-creator --username bancika@gmail.com/DIYLC/src/org/diylc/components/AbstractCurvedComponent.java
@@ -0,0 +1,106 @@
+package org.diylc.components;
+
+import java.awt.AlphaComposite;
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Composite;
+import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.geom.CubicCurve2D;
+
+import org.diylc.core.ComponentState;
+import org.diylc.core.Project;
+import org.diylc.core.annotations.EditableProperty;
+import org.diylc.utils.Constants;
+
+public abstract class AbstractCurvedComponent<T> extends AbstractTransparentComponent<T> {
+
+	private static final long serialVersionUID = 1L;
+
+	public static Color GUIDELINE_COLOR = Color.blue;
+
+	protected Point[] controlPoints = new Point[] {
+			new Point(-5 * Constants.GRID, -5 * Constants.GRID), new Point(0, -3 * Constants.GRID),
+			new Point(0, 3 * Constants.GRID), new Point(5 * Constants.GRID, 5 * Constants.GRID) };
+
+	protected Color color = getDefaultColor();
+
+	/**
+	 * Draws the specified curve onto graphics.
+	 * 
+	 * @param curve
+	 * @param g2d
+	 */
+	protected abstract void drawCurve(CubicCurve2D curve, Graphics2D g2d);
+	
+	protected abstract Color getDefaultColor();
+
+	@Override
+	public void drawIcon(Graphics2D g2d, int width, int height) {
+		g2d.setColor(getDefaultColor().darker());
+		g2d.setStroke(new BasicStroke(3));
+		CubicCurve2D curve = new CubicCurve2D.Double(1, height - 1, width / 4, height / 3,
+				3 * width / 4, 2 * height / 3, width - 1, 1);
+		g2d.draw(curve);
+		g2d.setColor(getDefaultColor());
+		g2d.setStroke(new BasicStroke(1));
+		g2d.draw(curve);
+	}
+
+	@Override
+	public void draw(Graphics2D g2d, ComponentState componentState, Project project) {
+		if (componentState == ComponentState.SELECTED || componentState == ComponentState.DRAGGING) {
+			g2d.setStroke(new BasicStroke(1, BasicStroke.CAP_ROUND, BasicStroke.JOIN_MITER, 10f,
+					new float[] { 2f, 3f }, 5f));
+			g2d.setColor(GUIDELINE_COLOR);
+			g2d.drawLine(controlPoints[0].x, controlPoints[0].y, controlPoints[1].x,
+					controlPoints[1].y);
+			g2d.drawLine(controlPoints[1].x, controlPoints[1].y, controlPoints[2].x,
+					controlPoints[2].y);
+			g2d.drawLine(controlPoints[2].x, controlPoints[2].y, controlPoints[3].x,
+					controlPoints[3].y);
+		}
+		CubicCurve2D curve = new CubicCurve2D.Double(controlPoints[0].x, controlPoints[0].y,
+				controlPoints[1].x, controlPoints[1].y, controlPoints[2].x, controlPoints[2].y,
+				controlPoints[3].x, controlPoints[3].y);
+
+		Composite oldComposite = g2d.getComposite();
+		if (alpha < MAX_ALPHA) {
+			g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1f * alpha
+					/ MAX_ALPHA));
+		}
+		// g2d.setColor(color.darker());
+		// g2d.setStroke(new BasicStroke(thickness));
+		// g2d.draw(path);
+		// g2d.setColor(color);
+		// g2d.setStroke(new BasicStroke(thickness - 2));
+		// g2d.draw(path);
+		drawCurve(curve, g2d);
+		g2d.setComposite(oldComposite);
+	}
+
+	@Override
+	public int getControlPointCount() {
+		return controlPoints.length;
+	}
+
+	@Override
+	public Point getControlPoint(int index) {
+		return controlPoints[index];
+	}
+
+	@Override
+	public void setControlPoint(Point point, int index) {
+		controlPoints[index].setLocation(point);
+	}
+
+	@EditableProperty
+	public Color getColor() {
+		return color;
+	}
+
+	public void setColor(Color color) {
+		this.color = color;
+	}
+
+}
---
+++  diy-layout-creator --username bancika@gmail.com/Library/src/org/diylc/components/connectivity/CurvedTrace.java
@@ -0,0 +1,48 @@
+package org.diylc.components.connectivity;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.geom.CubicCurve2D;
+
+import org.diylc.components.AbstractCurvedComponent;
+import org.diylc.core.IDIYComponent;
+import org.diylc.core.annotations.ComponentDescriptor;
+import org.diylc.core.annotations.EditableProperty;
+import org.diylc.core.measures.Size;
+import org.diylc.core.measures.SizeUnit;
+
+@ComponentDescriptor(name = "Curved Trace", author = "Branislav Stojkovic", category = "Connectivity", instanceNamePrefix = "CT", description = "Curved copper trace with two control points", zOrder = IDIYComponent.ABOVE_BOARD)
+public class CurvedTrace extends AbstractCurvedComponent<Size> {
+
+	private static final long serialVersionUID = 1L;
+
+	public static Color COLOR = Color.black;
+	public static Size SIZE = new Size(1d, SizeUnit.mm);
+
+	protected Size size = SIZE;
+
+	@Override
+	protected Color getDefaultColor() {
+		return COLOR;
+	}
+
+	@Override
+	protected void drawCurve(CubicCurve2D curve, Graphics2D g2d) {
+		int thickness = size.convertToPixels();
+		g2d.setStroke(new BasicStroke(thickness));
+		g2d.setColor(color);
+		g2d.draw(curve);
+	}
+
+	@EditableProperty(name = "Width")
+	@Override
+	public Size getValue() {
+		return size;
+	}
+
+	@Override
+	public void setValue(Size value) {
+		this.size = value;
+	}
+}
