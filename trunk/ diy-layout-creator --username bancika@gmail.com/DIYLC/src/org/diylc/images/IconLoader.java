78a4c22064fd378b367ca9fbf35ee4c6e3ed1ae6
bancika <bancika@813357f0-ed8a-11de-91ba-91c70a792394>
2011-01-14 13:21:44 +0000

    added help items

--- trunk/ diy-layout-creator --username bancika@gmail.com/DIYLC/src/org/diylc/images/IconLoader.java
+++ trunk/ diy-layout-creator --username bancika@gmail.com/DIYLC/src/org/diylc/images/IconLoader.java
@@ -14,14 +14,16 @@ public enum IconLoader {
 			"disk_blue.png"), SaveAs("save_as.png"), Exit("exit.png"), DocumentPlainYellow(
 			"document_plain_yellow.png"), PhotoScenery("photo_scenery.png"), LightBulbOn(
 			"lightbulb_on.png"), LightBulbOff("lightbulb_off.png"), NotebookAdd("notebook_add.png"), FormGreen(
-			"form_green.png"), Gears("gears.png"), About("about.png"), Donate("donate.gif"), WindowColors(
+			"form_green.png"), Gears("gears.png"), About("about.png"), WindowColors(
 			"window_colors.png"), WindowGear("window_gear.png"), NavigateCheck("navigate_check.png"), Undo(
 			"undo.png"), Error("error.png"), Warning("warning.png"), ZoomSmall("zoom_small.png"), MoveSmall(
 			"move_small.png"), Print("print.png"), PDF("pdf.png"), Excel("excel.png"), CSV(
 			"csv.png"), HTML("html.png"), Image("image.png"), Cut("cut.png"), Copy("copy.png"), Paste(
 			"paste.png"), Selection("selection.png"), BOM("bom.png"), BlackBoard("blackboard.png"), IdCard(
 			"id_card.png"), IdCardAdd("id_card_add.png"), Chest("chest.png"), Upload("upload.png"), Wrench(
-			"wrench.png"), Group("group.png"), Ungroup("ungroup.png"), TraceMask("trace_mask.png");
+			"wrench.png"), Group("group.png"), Ungroup("ungroup.png"), TraceMask("trace_mask.png"), Faq(
+			"faq.png"), Component("component.png"), Plugin("plugin.png"), Manual("manual.png"), Donate(
+			"donate.png"), Bug("bug.png");
 
 	protected String name;
 
