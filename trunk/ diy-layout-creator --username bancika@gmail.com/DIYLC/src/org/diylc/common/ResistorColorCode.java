1d3ca6dce2e8ac6ec7ba5f13f1d042adba962d6c
bancika <bancika@813357f0-ed8a-11de-91ba-91c70a792394>
2011-01-28 17:48:46 +0000

    fixed point visibility on curved trace added option to hide color bands on the resistor

--- trunk/ diy-layout-creator --username bancika@gmail.com/DIYLC/src/org/diylc/common/ResistorColorCode.java
+++ trunk/ diy-layout-creator --username bancika@gmail.com/DIYLC/src/org/diylc/common/ResistorColorCode.java
@@ -2,10 +2,13 @@ package org.diylc.common;
 
 public enum ResistorColorCode {
 
-	_4_BAND, _5_BAND;
-	
+	NONE, _4_BAND, _5_BAND;
+
 	@Override
 	public String toString() {
-		return name().substring(1).replace("_", " ").toLowerCase();
+		if (name().startsWith("_")) {
+			return name().substring(1).replace("_", " ").toLowerCase();
+		}
+		return name().substring(0, 1).toUpperCase() + name().substring(1).toLowerCase();
 	}
 }
