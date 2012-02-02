0db6bea1b2115578a53beaeab6a0c93e5fa524fc
bancika <bancika@813357f0-ed8a-11de-91ba-91c70a792394>
2012-02-02 04:26:59 +0000

    add more pin options

--- trunk/ diy-layout-creator --username bancika@gmail.com/diylc-library/src/org/diylc/components/semiconductors/DIL_IC.java
+++ trunk/ diy-layout-creator --username bancika@gmail.com/diylc-library/src/org/diylc/components/semiconductors/DIL_IC.java
@@ -343,7 +343,7 @@ public class DIL_IC extends AbstractTransparentComponent<String> {
 
 	public static enum PinCount {
 
-		_4, _6, _8, _10, _12, _14, _16, _18, _20, _22, _24, _26, _28, _30, _32;
+		_4, _6, _8, _10, _12, _14, _16, _18, _20, _22, _24, _26, _28, _30, _32, _34, _36, _38, _40, _42, _44, _46, _48, _50;
 
 		@Override
 		public String toString() {
