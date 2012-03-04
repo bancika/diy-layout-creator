6815a7b2d9e604796d7bad7b39fd133b4403652b
bancika <bancika@813357f0-ed8a-11de-91ba-91c70a792394>
2012-03-04 15:09:12 +0000

    Application breaks when resistance is set to 0 ohms

--- trunk/ diy-layout-creator --username bancika@gmail.com/diylc-core/src/org/diylc/core/measures/Resistance.java
+++ trunk/ diy-layout-creator --username bancika@gmail.com/diylc-core/src/org/diylc/core/measures/Resistance.java
@@ -32,36 +32,56 @@ public class Resistance extends AbstractMeasure<ResistanceUnit> {
 
 	public Color[] getColorCode(ResistorColorCode resistorColorCode) {
 		Color[] bands;
-		double base = getValue() * getUnit().getFactor();
-		int multiplier = 0;
-		while (base > (resistorColorCode == ResistorColorCode._4_BAND ? 99 : 999)) {
-			multiplier += 1;
-			base /= 10;
-		}
-		while (base < (resistorColorCode == ResistorColorCode._4_BAND ? 10 : 100)) {
-			multiplier -= 1;
-			base *= 10;
-		}
-		if (multiplier > 6 || multiplier < -2) {
-			// Out of range.
-			return new Color[] {};
-		}
-		switch (resistorColorCode) {
-		case _4_BAND:
-			bands = new Color[3];
-			bands[0] = COLOR_DIGITS[(int) (base / 10)];
-			bands[1] = COLOR_DIGITS[(int) (base % 10)];
-			bands[2] = COLOR_MULTIPLIER[multiplier + 2];
-			break;
-		case _5_BAND:
-			bands = new Color[4];
-			bands[0] = COLOR_DIGITS[(int) (base / 100)];
-			bands[1] = COLOR_DIGITS[(int) (base / 10 % 10)];
-			bands[2] = COLOR_DIGITS[(int) (base % 10)];
-			bands[3] = COLOR_MULTIPLIER[multiplier + 2];
-			break;
-		default:
-			bands = new Color[] {};
+		if (getValue() == 0) {
+			switch (resistorColorCode) {
+			case _4_BAND:
+				bands = new Color[3];
+				bands[0] = COLOR_DIGITS[0];
+				bands[1] = COLOR_DIGITS[0];
+				bands[2] = COLOR_MULTIPLIER[2];
+				break;
+			case _5_BAND:
+				bands = new Color[4];
+				bands[0] = COLOR_DIGITS[0];
+				bands[1] = COLOR_DIGITS[0];
+				bands[2] = COLOR_DIGITS[0];
+				bands[3] = COLOR_MULTIPLIER[2];
+				break;
+			default:
+				bands = new Color[] {};
+			}
+		} else {
+			double base = getValue() * getUnit().getFactor();
+			int multiplier = 0;
+			while (base > (resistorColorCode == ResistorColorCode._4_BAND ? 99 : 999)) {
+				multiplier += 1;
+				base /= 10;
+			}
+			while (base < (resistorColorCode == ResistorColorCode._4_BAND ? 10 : 100)) {
+				multiplier -= 1;
+				base *= 10;
+			}
+			if (multiplier > 6 || multiplier < -2) {
+				// Out of range.
+				return new Color[] {};
+			}
+			switch (resistorColorCode) {
+			case _4_BAND:
+				bands = new Color[3];
+				bands[0] = COLOR_DIGITS[(int) (base / 10)];
+				bands[1] = COLOR_DIGITS[(int) (base % 10)];
+				bands[2] = COLOR_MULTIPLIER[multiplier + 2];
+				break;
+			case _5_BAND:
+				bands = new Color[4];
+				bands[0] = COLOR_DIGITS[(int) (base / 100)];
+				bands[1] = COLOR_DIGITS[(int) (base / 10 % 10)];
+				bands[2] = COLOR_DIGITS[(int) (base % 10)];
+				bands[3] = COLOR_MULTIPLIER[multiplier + 2];
+				break;
+			default:
+				bands = new Color[] {};
+			}
 		}
 		return bands;
 	}
