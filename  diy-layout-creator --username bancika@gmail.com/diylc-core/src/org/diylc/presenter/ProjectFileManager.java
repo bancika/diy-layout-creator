18261eab6986bfa1a4606ed18fbad2b5ad215828
bancika <bancika@813357f0-ed8a-11de-91ba-91c70a792394>
2012-08-17 12:35:15 +0000

    Control number of "Recently Used" items from config.properties

---  diy-layout-creator --username bancika@gmail.com/diylc-core/src/org/diylc/presenter/ProjectFileManager.java
+++  diy-layout-creator --username bancika@gmail.com/diylc-core/src/org/diylc/presenter/ProjectFileManager.java
@@ -6,7 +6,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.Reader;
