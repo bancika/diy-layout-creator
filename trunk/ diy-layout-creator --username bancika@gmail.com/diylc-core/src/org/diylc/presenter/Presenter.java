d0bafb04e7d57cea3373bc47b4700ebb009a6b94
bancika <bancika@813357f0-ed8a-11de-91ba-91c70a792394>
2012-09-21 10:42:33 +0000

    Editable label color for leaded components

--- trunk/ diy-layout-creator --username bancika@gmail.com/diylc-core/src/org/diylc/presenter/Presenter.java
+++ trunk/ diy-layout-creator --username bancika@gmail.com/diylc-core/src/org/diylc/presenter/Presenter.java
@@ -58,7 +58,7 @@ public class Presenter implements IPlugInPort {
 	private static final Logger LOG = Logger.getLogger(Presenter.class);
 
 	public static final VersionNumber CURRENT_VERSION = new VersionNumber(3,
-			16, 0);
+			17, 0);
 	public static final String DEFAULTS_KEY_PREFIX = "default.";
 
 	public static final List<IDIYComponent<?>> EMPTY_SELECTION = Collections
