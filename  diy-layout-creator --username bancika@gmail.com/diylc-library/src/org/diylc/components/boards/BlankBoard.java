8c98e9dececa7686f6764baa92253321c41f679a
bancika <bancika@813357f0-ed8a-11de-91ba-91c70a792394>
2013-04-23 15:45:11 +0000

    Large components disappear when none of the edges is in the visible area

---  diy-layout-creator --username bancika@gmail.com/diylc-library/src/org/diylc/components/boards/BlankBoard.java
+++  diy-layout-creator --username bancika@gmail.com/diylc-library/src/org/diylc/components/boards/BlankBoard.java
@@ -1,8 +1,12 @@
 package org.diylc.components.boards;
 
 import java.awt.Graphics2D;
+import java.awt.Shape;
 
+import org.diylc.core.ComponentState;
 import org.diylc.core.IDIYComponent;
+import org.diylc.core.IDrawingObserver;
+import org.diylc.core.Project;
 import org.diylc.core.annotations.BomPolicy;
 import org.diylc.core.annotations.ComponentDescriptor;
 
@@ -18,6 +22,19 @@ public class BlankBoard extends AbstractBoard {
 		g2d.fillRect(2 / factor, 2 / factor, width - 4 / factor, height - 4 / factor);
 		g2d.setColor(BORDER_COLOR);
 		g2d.drawRect(2 / factor, 2 / factor, width - 4 / factor, height - 4 / factor);
+	}
+	
+	@Override
+	public void draw(Graphics2D g2d, ComponentState componentState,
+			boolean outlineMode, Project project,
+			IDrawingObserver drawingObserver) {
+		Shape clip = g2d.getClip();
+		if (checkPointsClipped(clip)
+				&& !clip.contains(firstPoint.x, secondPoint.y)
+				&& !clip.contains(secondPoint.x, firstPoint.y)) {
+			return;
+		}
+		super.draw(g2d, componentState, outlineMode, project, drawingObserver);
 	}
 	
 	@Override
