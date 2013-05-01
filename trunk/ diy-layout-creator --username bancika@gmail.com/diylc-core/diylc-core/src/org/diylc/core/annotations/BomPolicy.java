e9269f350b7309b03ef8065fa4abbaf8e49aa596
bancika <bancika@813357f0-ed8a-11de-91ba-91c70a792394>
2013-05-01 09:49:41 +0000

    undo

--- trunk/ diy-layout-creator --username bancika@gmail.com/diylc-core/diylc-core/src/org/diylc/core/annotations/BomPolicy.java
+++
@@ -1,7 +0,0 @@
-package org.diylc.core.annotations;
-
-public enum BomPolicy {
-	SHOW_ALL_NAMES,
-	SHOW_ONLY_TYPE_NAME,
-	NEVER_SHOW,
-}
--- trunk/ diy-layout-creator --username bancika@gmail.com/diylc-core/diylc-core/src/org/diylc/core/measures/Unit.java
+++
@@ -1,6 +0,0 @@
-package org.diylc.core.measures;
-
-public interface Unit {
-
-	double getFactor();
-}
