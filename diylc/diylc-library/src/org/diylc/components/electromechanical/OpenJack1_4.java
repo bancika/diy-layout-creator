10905a7f0aadae661f2fa25bd1bc135d980d971a
bancika <bancika@gmail.com>
2016-12-29 18:07:18 +0100

    cleanup

--- diylc/diylc-library/src/org/diylc/components/electromechanical/OpenJack1_4.java
+++ diylc/diylc-library/src/org/diylc/components/electromechanical/OpenJack1_4.java
@@ -4,7 +4,6 @@ import java.awt.AlphaComposite;
 import java.awt.Color;
 import java.awt.Composite;
 import java.awt.Graphics2D;
-import java.awt.Paint;
 import java.awt.Point;
 import java.awt.Shape;
 import java.awt.geom.AffineTransform;
