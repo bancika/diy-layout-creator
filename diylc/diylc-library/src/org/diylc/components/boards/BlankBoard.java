c37b0f307138ad9b511b6792122099cfd9006cef
bancika <bancika@gmail.com>
2017-04-27 17:15:38 +0200

    Ability to change PCB board shape from rectangular to oval

--- diylc/diylc-library/src/org/diylc/components/boards/BlankBoard.java
+++ diylc/diylc-library/src/org/diylc/components/boards/BlankBoard.java
@@ -1,8 +1,11 @@
 package org.diylc.components.boards;
 
+import java.awt.AlphaComposite;
+import java.awt.Composite;
 import java.awt.Graphics2D;
 import java.awt.Shape;
 
+import org.diylc.common.ObjectCache;
 import org.diylc.common.SimpleComponentTransformer;
 import org.diylc.core.ComponentState;
 import org.diylc.core.IDIYComponent;
@@ -10,6 +13,7 @@ import org.diylc.core.IDrawingObserver;
 import org.diylc.core.Project;
 import org.diylc.core.annotations.BomPolicy;
 import org.diylc.core.annotations.ComponentDescriptor;
+import org.diylc.core.annotations.EditableProperty;
 
 @ComponentDescriptor(name = "Blank Board", category = "Boards", author = "Branislav Stojkovic",
     zOrder = IDIYComponent.BOARD, instanceNamePrefix = "Board", description = "Blank circuit board",
@@ -17,6 +21,8 @@ import org.diylc.core.annotations.ComponentDescriptor;
 public class BlankBoard extends AbstractBoard {
 
   private static final long serialVersionUID = 1L;
+
+  private Type type = Type.SQUARE;
 
   @Override
   public void drawIcon(Graphics2D g2d, int width, int height) {
@@ -35,12 +41,55 @@ public class BlankBoard extends AbstractBoard {
         && !clip.contains(secondPoint.x, firstPoint.y)) {
       return;
     }
-    super.draw(g2d, componentState, outlineMode, project, drawingObserver);
+
+    g2d.setStroke(ObjectCache.getInstance().fetchBasicStroke(1));
+    if (componentState != ComponentState.DRAGGING) {
+      Composite oldComposite = g2d.getComposite();
+      if (alpha < MAX_ALPHA) {
+        g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1f * alpha / MAX_ALPHA));
+      }
+      g2d.setColor(boardColor);
+      if (getType() == Type.SQUARE)
+        g2d.fillRect(firstPoint.x, firstPoint.y, secondPoint.x - firstPoint.x, secondPoint.y - firstPoint.y);
+      else
+        g2d.fillOval(firstPoint.x, firstPoint.y, secondPoint.x - firstPoint.x, secondPoint.y - firstPoint.y);
+      g2d.setComposite(oldComposite);
+    }
+    // Do not track any changes that follow because the whole board has been
+    // tracked so far.
+    drawingObserver.stopTracking();
+    g2d.setColor(componentState == ComponentState.SELECTED || componentState == ComponentState.DRAGGING ? SELECTION_COLOR
+        : borderColor);
+    if (getType() == Type.SQUARE)
+      g2d.drawRect(firstPoint.x, firstPoint.y, secondPoint.x - firstPoint.x, secondPoint.y - firstPoint.y);
+    else
+      g2d.drawOval(firstPoint.x, firstPoint.y, secondPoint.x - firstPoint.x, secondPoint.y - firstPoint.y);
   }
 
   @Override
   public boolean getDrawCoordinates() {
     // Override to prevent editing.
     return super.getDrawCoordinates();
+  }
+
+  @EditableProperty(name = "Shape")
+  public Type getType() {
+    if (type == null) {
+      type = Type.SQUARE;
+    }
+    return type;
+  }
+
+  public void setType(Type type) {
+    this.type = type;
+  }
+
+  static enum Type {
+    ROUND, SQUARE;
+
+    @Override
+    public String toString() {
+      return name().substring(0, 1) + name().substring(1).toLowerCase();
+    }
   }
 }
