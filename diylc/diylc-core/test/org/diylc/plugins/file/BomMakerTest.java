a155aef59644dd693f567e0f957ffc4709ab1d70
Branislav Stojkovic <Branislav Stojkovic@Bancika-PC>
2019-02-25 11:10:36 +0100

    fix bom test

--- diylc/diylc-core/test/org/diylc/plugins/file/BomMakerTest.java
+++ diylc/diylc-core/test/org/diylc/plugins/file/BomMakerTest.java
@@ -25,6 +25,8 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import org.diylc.core.IDIYComponent;
@@ -48,11 +50,17 @@ public class BomMakerTest {
     List<BomEntry> bom = BomMaker.getInstance().createBom(components);
     assertNotNull(bom);
     assertEquals(2, bom.size());
+    Collections.sort(bom, new Comparator<BomEntry>() {
+
+      @Override
+      public int compare(BomEntry o1, BomEntry o2) {
+        return o1.getQuantity().compareTo(o2.getQuantity());
+      }});
     BomEntry entry1 = bom.get(0);
     assertEquals("something", entry1.getName());
-    assertEquals(2, (int) entry1.getQuantity());
+    assertEquals(1, (int) entry1.getQuantity());
     BomEntry entry2 = bom.get(1);
-    assertEquals("something", entry2.getName());
-    assertEquals(1, (int) entry2.getQuantity());
+    assertEquals("something, something", entry2.getName());
+    assertEquals(2, (int) entry2.getQuantity());
   }
 }
