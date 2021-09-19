6cc82ba9d7ea17d61b06d1a12321c8740b1b86bf
Bane Stojkovic <bancika@gmail.com>
2021-09-19 11:51:20 +0200

    Add shabang to run.sh

--- diylc/diylc-swing/run.sh
+++ diylc/diylc-swing/run.sh
@@ -1,1 +1,2 @@
-java -Xms512m -Xmx2048m -javaagent:lib/jar-loader.jar -Dorg.diylc.scriptRun=true -Dfile.encoding=UTF-8 -cp diylc.jar:lib org.diylc.DIYLCStarter
+#!/bin/bash
+java -Xms512m -Xmx2048m -javaagent:lib/jar-loader.jar -Dorg.diylc.scriptRun=true -Dfile.encoding=UTF-8 -cp diylc.jar:lib org.diylc.DIYLCStarter "$@"
