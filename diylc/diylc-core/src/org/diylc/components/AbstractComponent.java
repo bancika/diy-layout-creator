e369266d49561e5e90a83851410fd34ed6422c79
Bane Stojkovic <bancika@gmail.com>
2022-11-06 18:06:27 +0100

    Update gold color

--- diylc/diylc-core/src/org/diylc/components/AbstractComponent.java
+++ diylc/diylc-core/src/org/diylc/components/AbstractComponent.java
@@ -59,7 +59,7 @@ public abstract class AbstractComponent<T> implements IDIYComponent<T> {
   public static Color METAL_COLOR = Color.decode("#759DAF");
   public static Color LIGHT_METAL_COLOR = Color.decode("#EEEEEE");
   public static Color COPPER_COLOR = Color.decode("#DA8A67");
-  public static Color GOLD_COLOR = Color.decode("#DAA520");
+  public static Color GOLD_COLOR = Color.decode("#F4B301");
   public static Color FR4_COLOR = Color.decode("#6B9B6B");
   public static Color FR4_LIGHT_COLOR = Color.decode("#BABE71");
   public static Color PHENOLIC_COLOR = Color.decode("#F8EBB3");
