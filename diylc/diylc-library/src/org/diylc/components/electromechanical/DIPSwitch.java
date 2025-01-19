e84e5bf6820364b7012d8357b7b9c748595e6011
php4fan <php4fan@gmail.com>
2025-01-19 14:59:26 +0100

    Allow one-switch DIP switches
    I wonder if this alone will fix #859. Haven't tested

--- diylc/diylc-library/src/org/diylc/components/electromechanical/DIPSwitch.java
+++ diylc/diylc-library/src/org/diylc/components/electromechanical/DIPSwitch.java
@@ -530,7 +530,7 @@ public class DIPSwitch extends AbstractTransparentComponent<String> implements I
 
   public static enum SwitchCount {
 
-    _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12;
+    _1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12;
 
     @Override
     public String toString() {
