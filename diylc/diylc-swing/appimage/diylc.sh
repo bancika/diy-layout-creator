e7e9e889e1b047b517eaf57f05e2f4298743dca3
Branislav Stojkovic <bancika@gmail.com>
2025-03-07 16:47:34 +0100

    appimage

--- diylc/diylc-swing/appimage/diylc.sh
+++ diylc/diylc-swing/appimage/diylc.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
-HERE="$(dirname "$(readlink -f "$0")")"
+HERE="$(dirname \"$(readlink -f \"$0\")\")"
 
 # Set JRE path inside AppImage
-if [ -d "$HERE/usr/lib/jre" ]; then
-    JAVA_HOME="$HERE/../lib/jre"
-    PATH="$JAVA_HOME/bin:$PATH"
-    export JAVA_HOME PATH
+if [ -d "$HERE/jre" ]; then
+    export JAVA_HOME="$HERE/jre"
+    export PATH="$JAVA_HOME/bin:$PATH"
+    JAVA="$JAVA_HOME/bin/java"
+else
+    JAVA="$(which java)"
 fi
 
-cd /usr/bin
+cd "$HERE"
 
-exec "$JAVA_HOME/bin/java" -Xms512m -Xmx4096m -javaagent:lib/jar-loader.jar -Dorg.diylc.scriptRun=true -Dfile.encoding=UTF-8 -cp diylc.jar:lib --add-exports java.desktop/com.apple.eawt.event=ALL-UNNAMED  --add-exports java.desktop/com.apple.eio=ALL-UNNAMED --add-opens java.base/java.util=ALL-UNNAMED --add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.base/java.text=ALL-UNNAMED --add-opens java.desktop/java.awt=ALL-UNNAMED --add-opens java.desktop/java.awt.font=ALL-UNNAMED --add-opens java.desktop/java.awt.geom=ALL-UNNAMED  org.diylc.DIYLCStarter "$@"
+exec "$JAVA" \
+  -javaagent:"$HERE/../lib/lib/jar-loader.jar" \
+  -Xms128m \
+  -Xmx4096m \
+  -cp "$HERE/../lib/diylc.jar:$HERE/../lib/lib/*" \
+  --add-exports java.desktop/com.apple.eawt.event=ALL-UNNAMED \
+  --add-exports java.desktop/com.apple.eio=ALL-UNNAMED \
+  --add-opens java.base/java.util=ALL-UNNAMED \
+  --add-opens java.base/java.lang=ALL-UNNAMED \
+  --add-opens java.base/java.util=ALL-UNNAMED \
+  org.diylc.DIYLCStarter "$@"
