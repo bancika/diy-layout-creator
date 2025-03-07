9006e9bbc57ca5cd92d6ef631c596f674794baad
Bane Stojkovic <bancika@gmail.com>
2025-03-07 12:49:06 +0100

    Update GerberLayer.java
    remove mirroring

--- diylc/diylc-core/src/org/diylc/core/gerber/GerberLayer.java
+++ diylc/diylc-core/src/org/diylc/core/gerber/GerberLayer.java
@@ -8,11 +8,11 @@ public enum GerberLayer {
   Outline("Profile,NP", "gko", "Edge-Cuts", false, false),
   DrillNonPlated("NonPlated,1,2,NPTH", "drl", "NPTH", false, false),
   DrillPlated("Plated,1,2,PTH", "drl", "PTH", false, false),
-  CopperBot("Copper,L1,Bot", "gbl", "B_Cu", false, true),
+  CopperBot("Copper,L1,Bot", "gbl", "B_Cu", false, false),
   CopperTop("Copper,L1,Top", "gtl", "T_Cu", false, false),
-  SilkscreenBot("Legend,Bot", "gbo", "B_Silkscreen", false, true),
+  SilkscreenBot("Legend,Bot", "gbo", "B_Silkscreen", false, false),
   SilkscreenTop("Legend,Top", "gto", "T_Silkscreen", false, false),
-  SolderMaskBot("Soldermask,Bot", "gbs", "B_Mask", true, true),
+  SolderMaskBot("Soldermask,Bot", "gbs", "B_Mask", true, false),
   SolderMaskTop("Soldermask,Top", "gts", "T_Mask", true, false);
   
   private String function;
