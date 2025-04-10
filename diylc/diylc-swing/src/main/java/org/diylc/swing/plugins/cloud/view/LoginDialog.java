97674bac5cd8e80aed867da022ed191b64f6f2c7
Bane Stojkovic <bancika@gmail.com>
2025-04-10 21:17:44 +0200

    add login dialog

--- diylc/diylc-swing/src/main/java/org/diylc/swing/plugins/cloud/view/LoginDialog.java
+++ diylc/diylc-swing/src/main/java/org/diylc/swing/plugins/cloud/view/LoginDialog.java
@@ -25,6 +25,12 @@ import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.Cursor;
+import java.awt.Desktop;
+import java.awt.Color;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.net.URI;
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JComponent;
@@ -35,16 +41,23 @@ import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
+
+import org.apache.log4j.Logger;
+import org.diylc.lang.LangUtil;
+import org.diylc.swing.plugins.cloud.actions.LoginAction;
 import org.diylc.swingframework.ButtonDialog;
 
 public class LoginDialog extends ButtonDialog {
 
   private static final long serialVersionUID = 1L;
+  private static final String PASSWORD_RESET_URL = "http://www.diy-fever.com/diylc/api/v1/request_password_reset.php";
+
+  private static final Logger LOG = Logger.getLogger(LoginDialog.class);
 
   private JPanel mainPanel;
-
   private JTextField userNameField;
   private JPasswordField passwordField;
+  private JLabel forgotPasswordLabel;
 
   private String userName;
   private String password;
@@ -78,10 +91,10 @@ public class LoginDialog extends ButtonDialog {
       gbc.gridx = 0;
 
       gbc.gridy = 0;
-      mainPanel.add(new JLabel("User Name:"), gbc);
+      mainPanel.add(new JLabel(LangUtil.translate("User Name") + ":"), gbc);
 
       gbc.gridy = 1;
-      mainPanel.add(new JLabel("Password:"), gbc);
+      mainPanel.add(new JLabel(LangUtil.translate("Password") + ":"), gbc);
 
       gbc.gridx = 1;
       gbc.fill = GridBagConstraints.HORIZONTAL;
@@ -92,8 +105,33 @@ public class LoginDialog extends ButtonDialog {
 
       gbc.gridy = 1;
       mainPanel.add(getPasswordField(), gbc);
+      
+      // Add forgot password link
+      gbc.gridy = 2;
+      gbc.gridx = 1;
+      gbc.anchor = GridBagConstraints.LINE_END;
+      mainPanel.add(getForgotPasswordLabel(), gbc);
     }
     return mainPanel;
+  }
+
+  private JLabel getForgotPasswordLabel() {
+    if (forgotPasswordLabel == null) {
+      forgotPasswordLabel = new JLabel("<html><u>" + LangUtil.translate("Forgot Password?") + "</u></html>");
+      forgotPasswordLabel.setForeground(Color.BLUE);
+      forgotPasswordLabel.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+      forgotPasswordLabel.addMouseListener(new MouseAdapter() {
+        @Override
+        public void mouseClicked(MouseEvent e) {
+          try {
+            Desktop.getDesktop().browse(new URI(PASSWORD_RESET_URL));
+          } catch (Exception ex) {
+            LOG.error("Could not open password reset page", ex);
+          }
+        }
+      });
+    }
+    return forgotPasswordLabel;
   }
 
   private void refreshState() {
