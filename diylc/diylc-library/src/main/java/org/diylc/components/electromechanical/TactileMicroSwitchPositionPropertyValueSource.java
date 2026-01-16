7900128d7c4abd706ce4c0d0eb646a6896909704
Bane Stojkovic <bancika@gmail.com>
2026-01-16 20:03:01 +0100

    Tactile Micro Switch' editor doesn't show up on double click

--- diylc/diylc-library/src/main/java/org/diylc/components/electromechanical/TactileMicroSwitchPositionPropertyValueSource.java
+++ diylc/diylc-library/src/main/java/org/diylc/components/electromechanical/TactileMicroSwitchPositionPropertyValueSource.java
@@ -1,17 +1,31 @@
+/*
+ *
+ * DIY Layout Creator (DIYLC). Copyright (c) 2009-2025 held jointly by the individual authors.
+ *
+ * This file is part of DIYLC.
+ *
+ * DIYLC is free software: you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * DIYLC is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with DIYLC. If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ */
 package org.diylc.components.electromechanical;
 
-import org.diylc.common.PropertyWrapper;
-import org.diylc.components.guitar.LeverSwitch;
 import org.diylc.core.AbstractDynamicPropertyValueSource;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 public class TactileMicroSwitchPositionPropertyValueSource extends AbstractDynamicPropertyValueSource {
 
   @Override
   public List<Object> getAvailableValues() {
-    return List.of("Released", "Pressed");
+    return List.of(0, 1);
   }
 }
